8748385c1dab5bac32120f9b052b6259f8e56d74
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2026-01-08 17:50:48 +0100

    add DFS and BFS (really traversal and not search)

--- 19_29-DataStructures/Graph.ts
+++ 19_29-DataStructures/Graph.ts
@@ -1,3 +1,6 @@
+import { Stack } from "./Stack";
+import { Queue } from "./Queue";
+
 export default {};
 
 export class Graph<T> {
@@ -49,4 +52,102 @@ export class Graph<T> {
 
     return this.adjacencyList.delete(vertex);
   }
+
+  //   Recursive
+  //   depthFirstSearch(vertex: T, result: T[] = []): T[] {
+  //     result.push(vertex);
+  //     const vertexNeighbors = this.adjacencyList.get(vertex);
+
+  //     if (!vertexNeighbors?.length) return result;
+  //     for (const vertexNeighbor of vertexNeighbors) {
+  //       if (result.includes(vertexNeighbor)) continue;
+  //       this.depthFirstSearch(vertexNeighbor, result);
+  //     }
+  //     return result;
+  //   }
+
+  //   Iterative
+  depthFirstSearch(vertex: T): T[] {
+    const result: T[] = [vertex];
+    const stack = new Stack<T>();
+
+    let currentVertex: T | undefined = vertex;
+    while (currentVertex !== undefined) {
+      const vertexNeighbors = this.adjacencyList.get(currentVertex);
+      if (!vertexNeighbors?.length) continue;
+
+      for (const vertexNeighbor of vertexNeighbors) {
+        if (result.includes(vertexNeighbor)) continue;
+
+        result.push(vertexNeighbor);
+        stack.push(vertexNeighbor);
+      }
+
+      currentVertex = stack.pop();
+    }
+    return result;
+  }
+
+  breadthFirstSearch(vertex: T): T[] {
+    const result: T[] = [vertex];
+    const queue = new Queue<T>();
+
+    let currentVertex: T | null = vertex;
+    while (currentVertex !== null) {
+      const vertexNeighbors = this.adjacencyList.get(currentVertex);
+      if (!vertexNeighbors?.length) continue;
+
+      for (const vertexNeighbor of vertexNeighbors) {
+        if (result.includes(vertexNeighbor)) continue;
+
+        result.push(vertexNeighbor);
+        queue.enqueue(vertexNeighbor);
+      }
+
+      currentVertex = queue.dequeue();
+    }
+    return result;
+  }
 }
+
+// Graphs - Dijkstra Exercise
+// Create a constructor function for a WeightedGraph. It should inherit from the Graph constructor and have all the same methods except for adding an edge. Since weights will now be added with edges, the adjacency list should not only store the nodes which are connected to it but also the corresponding weight of the edge.
+
+// A PriorityQueue has been implemented for you. The PriorityQueue adds the LOWEST priority first (this is helpful for Dijkstra).
+
+// Implement the following method on the WeightedGraph.prototype
+
+// Dijkstra - this function should return an array with two values, the first being the total distance and the second an array of nodes which create the shortest path.
+
+// var g = new WeightedGraph()
+
+// g.addVertex('A');
+// g.addVertex('Z');
+// g.addVertex('C');
+// g.addVertex('D');
+// g.addVertex('E');
+// g.addVertex('H');
+// g.addVertex('Q');
+// g.addVertex('G');
+
+// g.addEdge('A', 'Z', 7)
+// g.addEdge('A', 'C', 8)
+
+// g.addEdge('Z', 'Q', 2)
+
+// g.addEdge('C', 'G', 4)
+
+// g.addEdge('D', 'Q', 8)
+
+// g.addEdge('E', 'H', 1)
+
+// g.addEdge('H', 'Q', 3)
+
+// g.addEdge('Q', 'C', 6)
+
+// g.addEdge('G', 'Q', 9)
+
+// g.Dijkstra('A','E') // ["A", "Z", "Q", "H", "E"]
+// g.Dijkstra('A','Q') // ["A", "Z", "Q"]
+// g.Dijkstra('A','G') // ["A", "C", "G"]
+// g.Dijkstra('A','D') // ["A", "Z", "Q", "D"]
